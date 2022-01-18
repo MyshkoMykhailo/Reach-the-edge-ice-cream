572252da9511e19a3c139e32b1fc6748bca2fec1
Oksana Saralidze <saralidze.ok@gmail.com>
2022-01-18 12:49:30 +0200

    clean modal window

--- src/js/modal-map.js
+++ src/js/modal-map.js
@@ -1,29 +1,15 @@
 (() => {
-  const mobileMap = document.querySelector('.js-map-container');
-  const openMapBtn = document.querySelector('.js-open-map');
-  const closeMapBtn = document.querySelector('.js-close-map');
-
-  const toggleMap = () => {
-    const isMapOpen =
-      openMapBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMapBtn.setAttribute('aria-expanded', !isMapOpen);
-    mobileMap.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMapOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-map-open]'),
+    closeModalBtn: document.querySelector('[data-modal-map-close]'),
+    modal: document.querySelector('[data-modal-map]'),
   };
 
-  openMapBtn.addEventListener('click', toggleMap);
-  closeMapBtn.addEventListener('click', toggleMap);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMap.classList.remove('is-open');
-    openMapBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
